bc21e58b033c00d2c8f95e5b37b5a862aa3b3634
rickytedjo <rickyputra.tedjo@gmail.com>
2024-03-06 16:14:04 +0700

    DB Facade Implementation

---
+++ app/Http/Controllers/LevelController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class LevelController extends Controller
+{
+    public function index(){
+        
+        $data = DB::select('select * from m_level');
+
+        return view('level',['data'=>$data]);
+    }
+}
