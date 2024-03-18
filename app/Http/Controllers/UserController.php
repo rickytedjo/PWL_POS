8192aefadbaf5a0880345114fde5e33321cdc69f
rickytedjo <rickyputra.tedjo@gmail.com>
2024-03-18 13:28:20 +0700

    Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,9 @@ class UserController extends Controller
     public function index(){
 
         $user = UserModel::findOr(20, ['username','nama'],function(){
+            
             abort(404);
+
         });
         return view('user', ['data'=>$user]);
     }
