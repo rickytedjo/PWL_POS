f963c23db6521cf09215bb7a2b306d55ef729f86
rickytedjo <rickyputra.tedjo@gmail.com>
2024-03-18 13:54:02 +0700

    Retrieving or Creating Models

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -6,23 +6,17 @@
     <body>
         <h1>Data User</h1>
         <table border="1" cellpadding="2" cellspacing="0">
-            <!--<tr>
+            <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
             </tr>
             <tr>
-                <td>//$data->user_id}}</td>
-                <td>\</td>
-                <td>\//$data->nama}}</td>
-                <td>\//$data->level_id}}</td>
-            </tr> -->
-            <tr>
-                <th>Jumlah Pengguna</th>
-            </tr>
-            <tr>
-                <td>{{ $data }}</td>
+                <td>{{$data->user_id}}</td>
+                <td>{{$data->username}}</td>
+                <td>{{$data->nama}}</td>
+                <td>{{$data->level_id}}</td>
             </tr>
         </table>
     </body>
